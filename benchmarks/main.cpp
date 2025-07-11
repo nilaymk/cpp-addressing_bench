305dbe942e60b20c396a973b34e0012e34b56c33
Nilay Kothari <nilaymk@outlook.com>
2025-07-12 00:18:36 +0200

    Refactor: Separate out printing and execution + output csv

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -7,8 +7,17 @@
 #include <unordered_set>
 #include <limits>
 #include <chrono>
+#include <format>
+#include <tuple>
+#include <utility>
+
+#include <iostream>
+#include <tuple>
+#include <utility>
+
 #include "src/bucketing_hash_map.h"
 #include "src/open_addressing_hash_map.h"
+
 
 void random_fill_vector(std::vector<uint64_t>& vec) {
     const size_t vec_size = vec.size();
@@ -27,150 +36,160 @@ void random_fill_vector(std::vector<uint64_t>& vec) {
     }
 }
 
-#define BENCHMARK(msg, f) \
-{ \
-   auto start = std::chrono::high_resolution_clock::now(); \
-    (f); \
-    const std::chrono::duration<double, std::milli> duration \
-        = std::chrono::high_resolution_clock::now() - start; \
-    std::cout << msg << duration.count(); \
-}\
 
 template<typename Map>
-void insert_elements(Map& map, std::vector<uint64_t>& values, size_t count) {
-    std::for_each_n(
-        values.begin(),
-        count,
-        [&map](auto n) {
-            map.insert(n, n);
+void insert_elements(Map& map, std::vector<uint64_t>& keys, size_t count) {
+    for (auto i = 0; i < count; ++i) {
+        map.insert(keys[i], keys[i]);
+    } 
+};
+
+template<typename Map>
+void look_up_elements(Map& map, std::vector<uint64_t>& keys, size_t count) {
+    for (auto i = 0; i < count; ++i) {
+        auto v = map.get(keys[i]);
+        if (!(v.has_value() && *v.value() == keys[i])) {
+            throw std::runtime_error("PANIC! Incorrect item !!");
         } 
-    );
+    };
+};
+struct Benchmarks {
+    void benchmark(
+        std::string const& category,
+        std::string const& name,
+        std::string const& description,
+        std::function<void ()> benchmarked_fn
+     ) {
+        auto start = std::chrono::high_resolution_clock::now();
+        benchmarked_fn();
+        const std::chrono::duration<double, std::milli> duration =
+            std::chrono::high_resolution_clock::now() - start;
+        this->results.emplace_back(BenchmarkItem{
+            category,
+            name,
+            description,
+            duration
+        });
+     }
+
+    struct BenchmarkItem {
+        std::string category;
+        std::string name;
+        std::string description;
+        const std::chrono::duration<double, std::milli> duration;
+    };
+
+    std::string name;
+    std::vector<BenchmarkItem> results;
+    Benchmarks(std::string const& name_) : name(name_) {}
 };
 
 
-template<
-    const size_t oa_capacity = 54917,
-    const size_t bucket_count = 54917
->
-void benchmark_insertions(std::vector<uint64_t>& data, const size_t data_count)
+template<typename T, typename V, size_t... I>
+void tuple_visit_impl(T&& t, V&& v, std::index_sequence<I...>)
 {
-    OpenAddressingHashMap<uint64_t, uint64_t, oa_capacity> open_addressing_map;
-    BucketingHashMap<uint64_t, uint64_t, bucket_count> bucketting_map;
-
-    std::cout << "  {" << std::endl;
-    std::cout << "    \"data_count\": " << data_count << "," << std::endl;
-    std::cout << "    \"bucket_addressing_load_factor\": "
-        << (data_count * 100.0) / bucketting_map.num_buckets() << ", " <<  std::endl;
-    std::cout << "    \"open_addressing_load_factor\": "
-        << (data_count * 100.0) / open_addressing_map.capacity() << ", " << std::endl;
-
-    BENCHMARK(
-        "    \"bucketting_addressing_time\": ",
-        insert_elements(bucketting_map, data, data_count)
-    );
-    std::cout << "," << std:: endl;
-    BENCHMARK(
-        "    \"open_addressing_time\": ",
-        insert_elements(open_addressing_map, data, data_count)
-    );
-    std::cout << "  }," << std::endl;
+    (..., v(std::get<I>(t)));
 }
 
-template<typename Map>
-void look_up_elements(Map& map, std::vector<uint64_t>& values, size_t count) {
-    std::for_each_n(
-        values.begin(),
-        count,
-        [&map](auto n) {
-            auto v = map.get(n);
-            if (!(v.has_value() && *v.value() == n)) {
-                throw std::runtime_error("PANIC! Incorrect item !!");
-            }
-        } 
-    );
-};
-
-template<
-    const size_t oa_capacity = 54917,
-    const size_t bucket_count = 54917
->
-void benchmark_lookups(
-    std::vector<uint64_t>& data,
-    const size_t data_count
-)
+template<typename T, typename V>
+void tuple_visit(T&& t, V&& v)
 {
-    OpenAddressingHashMap<uint64_t, uint64_t, oa_capacity> open_addressing_map;
-    insert_elements(open_addressing_map, data, data_count);
-
-    BucketingHashMap<uint64_t, uint64_t, bucket_count> bucketting_map;
-    insert_elements(bucketting_map, data, data_count);
-
-    std::cout << "  {" << std::endl;
-    std::cout << "    \"data_count\": " << data_count << "," << std::endl;
-    std::cout << "    \"bucket_addressing_load_factor\": "
-        << (data_count * 100.0) / bucketting_map.num_buckets() << ", " << std::endl;
-    std::cout << "    \"open_addressing_load_factor\": "
-        << (data_count * 100.0) / open_addressing_map.capacity() << ", " << std::endl;
-
-    BENCHMARK(
-        "    \"bucketting_addressing_time\": ",
-        look_up_elements(open_addressing_map, data, data_count)
-    );
-    std::cout << "," << std:: endl;
-    BENCHMARK(
-        "    \"open_addressing_time\": ",
-        look_up_elements(open_addressing_map, data, data_count)
-    );
-    std::cout << "  }," << std::endl;
+    tuple_visit_impl(std::forward<T>(t), std::forward<V>(v),
+        std::make_index_sequence<std::tuple_size<
+            typename std::decay<T>::type>::value>());
 }
-
 
 int main() {
-    std::cout << "Hello benches" << std::endl;
+    std::vector<uint64_t> input_data(1'00'000);
+    random_fill_vector(input_data);
 
-    std::vector<uint64_t> data(1'00'000);
-    random_fill_vector(data);
-    std::cout << "[" << std::endl;
+    Benchmarks benchmarks{"Hash Map Addressing Scheme Benchmarks"};
 
-    std::cout << "{\n  \"bench_category\": \"Insertion with increasing load factors\"," << std::endl;
-    std::cout << "  \"results\": [" << std::endl;
-    benchmark_insertions(data, 10000);
-    benchmark_insertions(data, 20000);
-    benchmark_insertions(data, 25000);
-    benchmark_insertions(data, 30000);
-    benchmark_insertions(data, 40000);
-    benchmark_insertions(data, 50000);
-    benchmark_insertions(data, 54916);
-    std::cout << "  ]\n}," << std::endl;
+    std::cout << "Category, Name, DataSize, Capacity, LoadFactor, Duration(ms)" << std::endl;
 
-    std::cout << "{\n  \"bench_category\": \"Look-up tests with increasing load factors\", " << std::endl;
-    std::cout << "  \"results\": [" << std::endl;
-    benchmark_lookups(data, 10000);
-    benchmark_lookups(data, 20000);
-    benchmark_lookups(data, 25000);
-    benchmark_lookups(data, 30000);
-    benchmark_lookups(data, 40000);
-    benchmark_lookups(data, 50000);
-    benchmark_lookups(data, 54916);
-    std::cout << "  ]\n}," << std::endl;
+    for (auto data_size : {10000, 20000, 25000, 30000, 40000, 50000, 54916}) {
+        {
+            OpenAddressingHashMap<uint64_t, uint64_t, 54917> open_addressing_map;
+            benchmarks.benchmark(
+                "Insertion",
+                std::format("Open Addressing ({})", open_addressing_map.capacity()),
+                std::format(
+                    "{}, {}, {}",
+                    data_size,
+                    open_addressing_map.capacity(),
+                    (double)data_size*100.0/open_addressing_map.capacity()
+                ),
+                [&] {
+                    insert_elements(open_addressing_map, input_data, data_size);
+                }
+            );
 
-    std::cout << "{\n  \"bench_category\": \"Insertion tests low bucket counts and increasing load factor\"," << std::endl;
-    std::cout << "  \"results\": [" << std::endl;  
-    benchmark_insertions<54917, 503>(data, 25000);
-    benchmark_insertions<54917, 503>(data, 30000);
-    benchmark_insertions<54917, 503>(data, 40000);
-    benchmark_insertions<54917, 503>(data, 50000);
-    benchmark_insertions<54917, 503>(data, 54916);
-    std::cout << "  ]\n}," << std::endl;
+            benchmarks.benchmark(
+                "Look Up",
+                std::format("Open Addressing ({})", open_addressing_map.capacity()),
+                std::format(
+                    "{}, {}, {}",
+                    data_size,
+                    open_addressing_map.capacity(),
+                    (double)data_size*100.0/open_addressing_map.capacity()
+                ),
+                [&] {
+                    look_up_elements(open_addressing_map, input_data, data_size);
+                }
+            );
+        }
 
-    std::cout << "{\n  \"bench_category\": \"Lookup tests low bucket counts and increasing load factor\"," << std::endl;
-    std::cout << "  \"results\": [" << std::endl; 
-    benchmark_lookups<54917, 503>(data, 25000);
-    benchmark_lookups<54917, 503>(data, 30000);
-    benchmark_lookups<54917, 503>(data, 40000);
-    benchmark_lookups<54917, 503>(data, 50000);
-    benchmark_lookups<54917, 503>(data, 54916);
-    std::cout << "  ]\n}," << std::endl;
+        {
+            auto varying_bucket_sized_maps = std::tuple {
+                BucketingHashMap<uint64_t, uint64_t, 503>{},
+                BucketingHashMap<uint64_t, uint64_t, 54917>{},
+            };
 
-    std::cout << "]" << std::endl;
+            tuple_visit(
+                varying_bucket_sized_maps,
+                [&](auto& bucket_map){
+                    benchmarks.benchmark(
+                        "Insertion",
+                        std::format("Bucket Addressing ({})", bucket_map.num_buckets()),
+                        std::format(
+                            "{}, {}, {}",
+                            data_size,
+                            bucket_map.num_buckets(),
+                            (double)data_size*100.0/bucket_map.num_buckets()
+                        ),
+                        [&] {
+                            insert_elements(bucket_map, input_data, data_size);
+                        }
+                    );
+                }
+            );
+
+            tuple_visit(
+                varying_bucket_sized_maps,
+                [&](auto& bucket_map){
+                    benchmarks.benchmark(
+                        "Look Up",
+                        std::format("Bucket Addressing ({})", bucket_map.num_buckets()),
+                        std::format(
+                            "{}, {}, {}",
+                            data_size,
+                            bucket_map.num_buckets(),
+                            (double)data_size*100.0/bucket_map.num_buckets()
+                        ),
+                        [&] {
+                            look_up_elements(bucket_map, input_data, data_size);
+                        }
+                    );
+                }
+            );
+        }
+    }
+
+    for (auto&& benchmark_item: benchmarks.results) {
+        std::cout
+            << benchmark_item.category << ", "
+            << benchmark_item.name << ", "
+            << benchmark_item.description << ", "
+            << benchmark_item.duration.count() << std::endl;
+    }
 }
